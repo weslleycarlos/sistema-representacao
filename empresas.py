7f3138e590a5f29868b67c14e65fe41687ada7dd
Weslley Carlos de Morais <weslley.wcm@pf.gov.br>
2025-04-09 14:01:14 -0300

    Corrigido Empresa.__init__ para aceitar catalogo, endereco, email e telefone

--- empresas.py
+++ empresas.py
@@ -1,9 +1,18 @@
-# empresas.py
 class Empresa:
-    def __init__(self, nome, tipo_grade):
+    def __init__(self, nome, tipo_grade, catalogo=None, endereco='', email='', telefone=''):
         self.nome = nome
         self.tipo_grade = tipo_grade
-        self.catalogo = {}
+        self.catalogo = catalogo if catalogo is not None else {}
+        self.endereco = endereco
+        self.email = email
+        self.telefone = telefone
+
+    def adicionar_item(self, codigo, descritivo, valor, tamanhos):
+        self.catalogo[codigo] = {
+            "descritivo": descritivo,
+            "valor": valor,
+            "tamanhos": tamanhos
+        }
 
     def get_item(self, codigo):
-        return self.catalogo.get(codigo, None)
+        return self.catalogo.get(codigo)
